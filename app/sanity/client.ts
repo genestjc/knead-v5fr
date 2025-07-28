e177c130ccb23491945a3963635c429342a9ef7c
genestjc <99754098+genestjc@users.noreply.github.com>
2025-07-28 00:30:56 -0400

    Add files via upload

--- app/sanity/client.ts
+++ app/sanity/client.ts
@@ -1,21 +1,8 @@
 import { createClient } from "next-sanity"
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production"
-const apiVersion = "2023-05-03" // Use proper date format
-
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: true,
-  token: process.env.SANITY_API_READ_TOKEN,
-})
-
-export const adminClient = createClient({
-  projectId,
-  dataset,
-  apiVersion,
+  projectId: "cs0gtnjr",
+  dataset: "production",
+  apiVersion: "2024-01-01",
   useCdn: false,
-  token: process.env.SANITY_API_WRITE_TOKEN,
 })
