5f4c587392a4d50b369dbc27068fa60d35343fe7
genestjc <99754098+genestjc@users.noreply.github.com>
2026-01-28 12:57:55 -0500

    Add TypeScript definitions for key sharer types

---
+++ app/chat-test/key-sharer-types.d.ts
@@ -0,0 +1,11 @@
+declare global {
+  interface Window {
+    KEY_SHARER_AUTO_MODE?: boolean;
+    KEY_SHARER_PRIVATE_KEY?: string;
+    KEY_SHARER_CONNECTED?: boolean;
+    KEY_SHARER_ATTEMPTED?: boolean;
+    KEY_SHARER_ERROR?: string;
+  }
+}
+
+export {};
