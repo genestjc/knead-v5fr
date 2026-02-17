b99b878b5ed6042f638703e300c335827756ab13
genestjc <99754098+genestjc@users.noreply.github.com>
2026-02-17 12:06:05 -0500

    Refactor TOWNS_CONFIG to remove RPC dependency

--- lib/towns-config.ts
+++ lib/towns-config.ts
@@ -1,13 +1,3 @@
 import { townsEnv } from '@towns-protocol/sdk';
-import { getAllRpcEndpoints } from '@/thirdweb-client';
 
-const AVAILABLE_RPCS = getAllRpcEndpoints();
-const BASE_RPC = AVAILABLE_RPCS[Math.floor(Math.random() * AVAILABLE_RPCS.length)];
-
-const townsEnvWithRpc = townsEnv({
-  env: {
-    BASE_MAINNET_RPC_URL: BASE_RPC,
-  }
-});
-
-export const TOWNS_CONFIG = townsEnvWithRpc.makeTownsConfig('omega');
+export const TOWNS_CONFIG = townsEnv().makeTownsConfig('omega');
